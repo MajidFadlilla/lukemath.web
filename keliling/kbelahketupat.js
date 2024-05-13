5eab1129c93848d97b7ec47a50c64b4f5c288ea3
MajidFadlilla <168385513+MajidFadlilla@users.noreply.github.com>
2024-05-13 14:18:45 +0700

    Delete keliling directory

--- keliling/kbelahketupat.js
+++
@@ -1,4 +0,0 @@
-let sisi = prompt(`masukkan sisi (cm)`)
-const kelilingBelahKetupat = 4 * sisi
-
-alert(`dengan panjang sisi sepanjang ${sisi}cm, maka keliling belah ketupat dengan menggunakan rumus (K= 4s) adalah = ${kelilingBelahKetupat}cm`)
--- keliling/kjajargenjang.js
+++
@@ -1,7 +0,0 @@
-let a = Number(prompt(`masukkan panjang sisi mendatar (cm)`));
-let b =  Number(prompt("masukkan panjang sisi miring (cm)"));
-let kelilingJajarGenjang = 2 * (a + b);
-
-alert(`dengan panjang sisi mendatar ${a}cm dan panjang sisi miring ${b}cm, maka keliling jajar genjang dengan menggunakan rumus (K= 2a + 2b) adalah = ${kelilingJajarGenjang}cm`);
-
- console.log(kelilingJajarGenjang);
