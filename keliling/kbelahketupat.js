7064c3141df8a6aef931fb9921b89f3709aaf96a
MajidFadlilla <168385513+MajidFadlilla@users.noreply.github.com>
2024-05-15 13:35:59 +0700

    Delete keliling directory

--- keliling/kbelahketupat.js
+++
@@ -1,7 +0,0 @@
-let sisi = prompt(`masukkan sisi (cm)`)
-const kelilingBelahKetupat = 4 * sisi
-
-alert(`dengan panjang sisi sepanjang ${sisi}cm, maka keliling belah ketupat dengan menggunakan rumus (K= 4 x Sisi) adalah =
-Keliling Belah Ketupat = 4 x sisi  
-Keliling Belah Ketupat = 4 x ${sisi}cm 
-Keliling Belah Ketupat = ${kelilingBelahKetupat}cm`)
